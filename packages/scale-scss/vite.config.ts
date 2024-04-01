d511ade5ad37c7146d0baaa611d9f99de146b08c
David S. Garcia (Azæl) <azael@dragonspark.io>
2024-03-31 20:07:59 -0500

    chore(scale-scss): Initialize scss generation package
    A new package, scale-scss, has been added. This package is designed to dynamically generate SCSS stylesheets from JavaScript schemas for efficient and scalable styling workflows. The initial setup includes the creation of .gitignore, package.json, README.md, tsconfig.json and vite.config.ts files. The package.json file contains scripts for building and testing along with a list of dependencies required by the project. A README.md file has also been created to provide an overview of the project and instructions on how to get started.

---
+++ packages/scale-scss/vite.config.ts
@@ -0,0 +1,69 @@
+/// <reference types="vitest" />
+
+import license from 'rollup-plugin-license';
+import dts from 'vite-plugin-dts';
+import { codecovVitePlugin } from '@codecov/vite-plugin';
+import { defineConfig } from 'vite';
+
+const PKG_UNIT = 'packages';
+const PKG_NAME = 'scale-scss';
+const PKG_EXTERNAL_DEPS = ['prettier'];
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: `../../node_modules/.vite/${PKG_UNIT}/${PKG_NAME}`,
+  plugins: [
+    {
+      ...license({
+        sourcemap: true,
+        banner: {
+          commentStyle: 'ignored',
+          content: {
+            file: '../../LICENSE_BANNER',
+            encoding: 'utf-8'
+          }
+        }
+      }),
+      enforce: 'pre'
+    },
+    dts({
+      entryRoot: 'src',
+      rollupTypes: true,
+      compilerOptions: {
+        removeComments: true
+      }
+    }),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env['CODECOV_TOKEN'] !== undefined,
+      uploadToken: process.env['CODECOV_TOKEN'],
+      bundleName: `@draconic/${PKG_NAME}`
+    })
+  ],
+  build: {
+    outDir: './dist',
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true
+    },
+    lib: {
+      entry: 'src/index.ts',
+      fileName: 'index',
+      formats: ['es', 'cjs']
+    },
+    emptyOutDir: true,
+    rollupOptions: {
+      external: PKG_EXTERNAL_DEPS
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'node',
+    include: ['tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      enabled: true,
+      reportsDirectory: `../../coverage/${PKG_UNIT}/${PKG_NAME}`,
+      provider: 'v8'
+    }
+  }
+});
